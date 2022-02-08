e0cf7d2609637c23010c44936cb5450996351d29
sagarbhandari <sagarvandari1@gmail.com>
2022-02-08 18:52:25 +0545

    feat: articles create and read

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,40 @@
+# frozen_string_literal: true
+
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show]
+  def index
+    @articles = Article.all
+  end
+
+  def show; end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    article = Article.new(article_params)
+    if article.save
+      article.save
+      redirect_to articles_path
+    else
+      render 'new'
+    end
+  end
+
+  def delete; end
+
+  def edit; end
+
+  def update; end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  def find_article
+    @article = Article.find_by_id(params[:id])
+  end
 end

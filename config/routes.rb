ff43e104f2f05dabfbb5798b6ac32de0f68a6a9e
sagarbhandari <sagarvandari1@gmail.com>
2022-02-08 21:32:26 +0545

    build:comment controller and respective routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@
 
 Rails.application.routes.draw do
   resources :articles
+  resources :comments
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'articles#index'
+
 end

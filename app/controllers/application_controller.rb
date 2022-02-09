e6bfe71ae9f287daccd4c19968747709b3fee7e5
sagarbhandari <sagarvandari1@gmail.com>
2022-02-09 15:49:11 +0545

    fix:make data-tubo:false to all form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-class ApplicationController < ActionCont
+class ApplicationController < ActionController::Base
   def after_sign_in_path_for
-    articles_path
+    'articles/index'
   end
 end

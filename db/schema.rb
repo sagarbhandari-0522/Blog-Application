d2e6ec0c1ccaea2daf8cecaf84f524eb0863474e
sagarbhandari <sagarvandari1@gmail.com>
2022-02-09 23:23:06 +0545

    refactor:adding extra field in devise signup view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_09_132007) do
+ActiveRecord::Schema.define(version: 2022_02_09_144429) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2022_02_09_132007) do
     t.datetime "locked_at", precision: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true

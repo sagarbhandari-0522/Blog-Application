32cdfef6175495f8a81f480c96a595d94d3469d3
sagarbhandari <sagarvandari1@gmail.com>
2022-02-11 09:49:10 +0545

    build add dashboard controller and its dependency

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments
   end
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'users#index'
+  root 'dashboards#index'
 end

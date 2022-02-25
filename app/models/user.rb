ec18b73ada1c19056d9ec4e788b76f0fe288b6eb
sagarbhandari <sagarvandari1@gmail.com>
2022-02-25 17:51:45 +0545

    feat:user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_one_attached :avatar
   rolify
   after_create :assign_default_role
+  validates :email, presence: true, uniqueness: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable

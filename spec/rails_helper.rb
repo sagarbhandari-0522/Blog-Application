a3ae051b070913330cdda70c99706eea44a421fa
sagarbhandari <sagarvandari1@gmail.com>
2022-02-27 20:55:49 +0545

    feat :factory bot setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,14 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'factory_bot_rails'
+require 'support/factory_bot'
+require 'devise'
+require 'support/devise_request_spec_helpers'
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -61,4 +66,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include DeviseRequestSpecHelpers, type: :request
 end
